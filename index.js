ae9fe275030e4dd5a558b5eb82e51d5c4e27b83d
discoduckling <mitalee.n.desai@gmail.com>
2018-03-31 18:59:21 -0600

    Finish adding authentication routes, cookie exchange, and User schema

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 3 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
+
 
 
 const PORT = process.env.PORT || 5000;
